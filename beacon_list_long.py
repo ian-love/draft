00bc38a0b3d36786ae31033486e0571d9a8c8511
ian-love <39064738+ian-love@users.noreply.github.com>
2019-07-09 10:20:55 +0100

    Add files via upload
    Define your Meridian location ID under meridianLocation within the file beacons_list_long.py.  State the choice of location ID with testloc.  Finally ensure demo_API.py is in the root directory of python to allow the import statement to succeed.
    
    All tested using python 3.7.1

---
+++ beacon_list_long.py
@@ -0,0 +1,158 @@
+########################################################################################################################
+#
+# 11mar 2019
+# script to list beacon mac, associated map and beacon name
+# "C:\DATA\OneDrive - Hewlett Packard Enterprise\meridian\scripts\working\beacon_list.py"
+
+	# // start //
+	# EC24B830BE75 beacon is on 5639445604728832 with a name of Test
+	# 546C0E2A1359 beacon is on 5639445604728832 with a name of None
+	# 546C0E2A1351 beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E2A1267 beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E2A105D beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E2A1306 beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E159123 beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E1599B1 beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E159118 beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E170506 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A120E beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A1074 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A14FB beacon is on 5715999101812736 with a name of None
+	# 546C0E2A14E3 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E159162 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E170373 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A133E beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A1365 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A1374 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A1249 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A14BC beacon is on 5715999101812736 with a name of None
+	# 546C0E1707A6 beacon is on 5715999101812736 with a name of None
+	# 546C0E15910A beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E170790 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A120F beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A1239 beacon is on 5715999101812736 with a name of None
+	# 546C0E2A104E beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E157B41 beacon is on 5715999101812736 with a name of None
+	# 546C0E2A14D2 beacon is on 5715999101812736 with a name of None
+	# 546C0E2A148A beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A14FC beacon is on 5715999101812736 with a name of None
+	# 546C0E2A1070 beacon is on 5715999101812736 with a name of None
+	# 546C0E2A1240 beacon is on 5715999101812736 with a name of None
+	# 546C0E15740A beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A103F beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A160A beacon is on 5715999101812736 with a name of _Permanent
+	# 546C0E1707CA beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A14A2 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A125F beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A1320 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A1028 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A14C7 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E156BF5 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A126C beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E157424 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A103A beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A1046 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A1215 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A1231 beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E2A107D beacon is on 5715999101812736 with a name of Permanent
+	# 546C0E15B3B9 beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E2AF282 beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E2AF46F beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E2AF9BA beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E158A71 beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E2A105B beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E2AF9DB beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E2AF464 beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E15F666 beacon is on 5639445604728832 with a name of Permanent
+	# 546C0E159982 beacon is on 5639445604728832 with a name of None
+	# 546C0E170785 beacon is on 5639445604728832 with a name of None
+	# 546C0E2AF421 beacon is on 5639445604728832 with a name of None
+	# 546C0E15998D beacon is on 5639445604728832 with a name of None
+	# 546C0E2A1076 beacon is on 5639445604728832 with a name of None
+	# 546C0E2AF1ED beacon is on 5639445604728832 with a name of None
+	# 546C0E2AF47F beacon is on 5639445604728832 with a name of None
+	# 546C0E2AF1EA beacon is on 5639445604728832 with a name of None
+	# 546C0E2AF1C8 beacon is on 5639445604728832 with a name of None
+	# 546C0E2AF3E7 beacon is on 5639445604728832 with a name of None
+	# 546C0E1599BC beacon is on 5639445604728832 with a name of None
+	# 546C0E15BACD beacon is on 5639445604728832 with a name of None
+	# 546C0E1599EE beacon is on 5639445604728832 with a name of None
+	# 546C0E2A1210 beacon is on 5639445604728832 with a name of None
+	# 546C0E159138 beacon is on 5639445604728832 with a name of None
+	# 546C0E15B3F5 beacon is on 5639445604728832 with a name of None
+	# 546C0E2AF9E7 beacon is on 5715999101812736 with a name of None
+	# 546C0E2AFB14 beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF2F6 beacon is on 5715999101812736 with a name of None
+	# 546C0E17079F beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF42F beacon is on 5715999101812736 with a name of None
+	# 546C0E2A1480 beacon is on 5715999101812736 with a name of None
+	# 546C0E2A14C0 beacon is on 5715999101812736 with a name of None
+	# 546C0E1707C2 beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF9B0 beacon is on 5715999101812736 with a name of None
+	# 546C0E2A14D1 beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF47B beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF994 beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF456 beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF3C8 beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF2BD beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF2EA beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF2AE beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF408 beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF460 beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF9E0 beacon is on 5715999101812736 with a name of None
+	# 546C0E2A1048 beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF1D8 beacon is on 5715999101812736 with a name of None
+	# 546C0E170915 beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF1BA beacon is on 5715999101812736 with a name of None
+	# 546C0E2AF9A5 beacon is on 5715999101812736 with a name of None
+	# // end //
+########################################################################################################################
+# Load the meridianAPI.py module
+import requests
+from demo_API import *
+
+# Meridian location ID dictionary of all locations I'm authorized in
+meridianLocation = {
+"TSS_hague":"6695066221936640",
+"my_site":"5158295536926720",
+"cspo":"5794256550100992",
+"reimagine":"5204712926740480",
+"atm":"5819981480067072",
+"Terna" : "5673887759335424",
+"TSS_2019":"6314911219056640",
+"lyngsoe":"5200230666731520",
+"jbs" : "4518368792543232"}
+
+# list of endpoints
+endpoints = {
+"beacons":"/beacons?page_size=500",
+"placemarks":"/placemarks",
+"token":"/tokens",
+"login":"/login",
+"logout":"/logout",
+"locations":"/locations",
+"maps":"/maps",
+"organizations":"/organizations"}
+
+# URLs will be needed to communicate with Meridian's API on the regular server
+apiURI = "https://edit.meridianapps.com/api/locations/"
+loginURI = "https://edit.meridianapps.com/api/login"
+
+# URLs will be needed to communicate with Meridian's API on the EU server
+eu_apiURI = "https://edit.meridianapps.com/api/locations/"
+eu_loginURI = "https://edit.meridianapps.com/api/login"
+
+# which site ??
+testloc = meridianLocation["jbs"]
+tokid = MeridianAPI.reqtoken(loginURI)
+
+
+beaconEndpoint = endpoints["beacons"]
+beaconURI = MeridianAPI(apiURI, testloc, beaconEndpoint)
+print (beaconURI)
+
+beacons = requests.get(beaconURI)
+locbeacons = beacons.json()['results']
+
+for i in range(len(locbeacons)):
+		 print(locbeacons[i]['mac'],'beacon is on',locbeacons[i]['map'],'with a name of',locbeacons[i]['name'])
---
+++ demo_API.py
@@ -0,0 +1,72 @@
+########################################################################################################################
+#
+# demo_API.py
+#
+# Dump all my common stuff here and pull it in to any demo python files
+#
+########################################################################################################################
+# Here's a list of available Meridian API
+#
+# https:/edit.meridianapps.com/api
+# https:/edit.meridianapps.com/api/locations
+# https:/edit.meridianapps.com/api/beacons/manage/auth?app_id={location_id}
+# https:/edit.meridianapps.com/api/{location_id}/asset-beacons
+# https:/edit.meridianapps.com/api/{location_id}/placemarks
+# https://edit.meridianapps.com/api/locations/{Location ID}/placemarks/{Placemark ID}/image
+#
+#
+import requests 
+from requests.auth import HTTPBasicAuth 
+import unicodecsv as csv 
+import base64 
+# Set variables for global use
+username ='ian.love@hpe.com'
+password ='love2u'
+mauth = HTTPBasicAuth(username, password)
+
+class MeridianAPI:
+	def __init__(self, apiuri, meridianlocation="", apiname="", location_id=""):
+		self.apiuri = apiuri
+		self.meridianlocation = meridianlocation
+		self.apiname = apiname
+		self.location_id = location_id
+		
+	def __str__(self):
+		formURI = self.apiuri+self.meridianlocation+self.apiname+self.location_id
+		return formURI
+	
+	def reqtoken(self):
+		mytoken = requests.post(self,
+                           {'password': password, 'email': username})
+		print('\n\nMy Personal token for this session:', mytoken.json()['token'])
+		return mytoken.json()['token']
+	
+	def mylocs(locuri):
+		loc = requests.get(locuri,params=None,auth=mauth)
+		return loc.json()['results']
+		
+	def convcsv(filename="", tocsv=[]):
+		keys = tocsv.keys()
+		with open(filename,'w') as csvfile:
+			dict_writer = csv.DictWriter(csvfile, keys)
+			dict_writer.writeheader()
+			dict_writer.writerows(tocsv)
+			
+	def beacons(beaconsuri):
+		numOfBeacons = requests.get(beaconsuri)
+		print(numOfBeacons.url)
+		print(numOfBeacons.json()['results'])
+		typeOfBeacons = numOfBeacons.json()['results']
+		print("'\n\n'{0} {1} {2}".format('There are',len(typeOfBeacons),'beacons'))
+		for j in range(len(typeOfBeacons)):
+			print('Beacon: ','type', typeOfBeacons[j]['type'],'MAC', typeOfBeacons[j]['mac'],'firmware is', typeOfBeacons[j]['firmware_a_version'],'hardware type',
+				typeOfBeacons[j]['hw_type'], typeOfBeacons[j]['timestamp'])
+		return typeOfBeacons
+		
+	def placemarks(placemarksuri):
+		placemarks = requests.get(placemarksuri)
+		locplacemarks = placemarks.json()['results']
+		print("'\n\n'{0} {1} {2}.".format('This location has',len(locplacemarks),'placemarks'))
+		for i in range(len(locplacemarks)):
+			print('Placemark: ', locplacemarks[i]['name'],'is landmark ', locplacemarks[i]['landmark'],' x coordinate: ', locplacemarks[i]['x'], ' y coordinate: ', locplacemarks[i]['y'])
+		return locplacemarks
